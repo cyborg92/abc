08c2aaacbacfd0ed6a519e6d9002b72a5ccd905f
Rachit Handa <rachithanda93@yahoo.com>
2021-10-12 02:01:54 +0530

    building wrapper components

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,14 +1,25 @@
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetups";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="learn react"></Todo>
-      <Todo text="Master react"></Todo>
-      <Todo text="Explore react"></Todo>
-      
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage></AllMeetupsPage>
+        </Route>
+        <Route path="/new-meetups">
+          <NewMeetupsPage></NewMeetupsPage>
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage></FavoritesPage>
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
---
+++ react-practice/src/components/meetups/MeetupList.js
@@ -0,0 +1,21 @@
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
+
+function MeetupList(props) {
+  return (
+    <ul className={classes.list}>
+      {props.items.map((meetup) => {
+        return <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          description={meetup.description}
+          address={meetup.address}
+        ></MeetupItem>;
+      })}
+    </ul>
+  );
+}
+
+export default MeetupList;
